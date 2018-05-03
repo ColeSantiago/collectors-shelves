c0dbb638f274ada45cb02328411b7133c760b0db
Cole Santiago <colesantiago@Coles-MBP.fios-router.home>
2018-05-02 21:06:50 -0400

    heroku upload

--- models/user_collection.js
+++ models/user_collection.js
@@ -8,7 +8,6 @@ module.exports = (sequelize, DataTypes) => {
   user_collection.associate = function(models) {
     // associations can be defined here
     user_collection.hasMany(models.collection_photos);
-    user_collection.hasMany(models.collection_keywords);
     user_collection.belongsTo(models.user, {foreignKey : 'userId'});
   };
   return user_collection;
